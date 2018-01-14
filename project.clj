e573e25374997fce40c495f02d7f92d164f705c4
macleginn <dsnikolaev@gmail.com>
2018-01-14 17:11:08 +0200

    Create local Maven repo

--- project.clj
+++ project.clj
@@ -5,7 +5,12 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.3"]
-                 [environ "1.1.0"]]
+                 [environ "1.1.0"]
+                 [jsesh/jsesh "6.5.5"]
+                 [jseshGlyphs/jseshGlyphs "6.5.5"]
+                 [qenherkhopeshefUtils/qenherkhopeshefUtils "6.5.5"]
+                 [java-cup/java-cup "11b"]]
+  :repositories {"local" "file:lib"}
   :main hieroglyphs.web
   :min-lein-version "2.0.0"
   :heroku {:app-name "pure-sierra-27254"}
@@ -13,7 +18,7 @@
             [lein-heroku "0.5.3"]]
   :hooks [environ.leiningen.hooks]
   :java-source-paths ["src/java" "test/java"]
-  :resource-paths ["resources/jsesh-6.5.5.jar" "resources/jseshGlyphs-6.5.5.jar" "resources/java-cup-11b-runtime.jar" "resources/java-cup-11b.jar" "resources/qenherkhopeshefUtils-6.5.5.jar"]
+  ;; :resource-paths ["resources/jsesh-6.5.5.jar" "resources/jseshGlyphs-6.5.5.jar" "resources/java-cup-11b-runtime.jar" "resources/java-cup-11b.jar" "resources/qenherkhopeshefUtils-6.5.5.jar"]
   :uberjar-name "hieroglyphs-standalone.jar"
   :profiles {:production {:env {:production true}}
              :uberjar {:aot :all}})
